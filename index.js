a239484edab4d0b3ca54ba9a38ef174634a890c3
vishnusoman-in <vishnusoman.pro@gmail.com>
2022-12-29 04:45:35 -0800

    fix: add directory assets and unique filename set

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const storage = multer.diskStorage({
   },
 
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, Date.now() + file.originalname);
   },
 
 }); // this is from github from multer
